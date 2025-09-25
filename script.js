6111334b7bc0e16d4e734a4127c5a6ff84aeb65a
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-09-25 17:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here!
+let list = document.getElementById("infi-list");
+let count = 0;
 
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    count++;
+    let li = document.createElement("li");
+    li.innerText = "Item " + count;
+    list.appendChild(li);
+  }
+}
+addItems(10);
+window.onscroll = function () {
+  if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+    addItems(2);
+  }
+};
